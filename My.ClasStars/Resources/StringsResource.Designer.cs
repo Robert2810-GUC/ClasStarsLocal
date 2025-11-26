4880daa9a732926818a82df03952f435f935636d
Robert2810-GUC <mgdfr2810@gmail.com>
2025-11-26 10:33:19 +0530

    current change

--- My.ClasStars/Resources/StringsResource.Designer.cs
+++ My.ClasStars/Resources/StringsResource.Designer.cs
@@ -19,7 +19,7 @@ namespace My.ClasStars.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class StringsResource {
@@ -417,6 +417,15 @@ namespace My.ClasStars.Resources {
         public static string selectionmessage {
             get {
                 return ResourceManager.GetString("selectionmessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to ID.
+        /// </summary>
+        public static string StudentList_PersonID {
+            get {
+                return ResourceManager.GetString("StudentList_PersonID", resourceCulture);
             }
         }
         
